1d3c51d59aaa7b387083a5dc3a9f7fab6a9b4314
Markus Stäuble <staeuble@gmail.com>
2020-04-14 20:50:02 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,12 +12,19 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(2200)
     basic.showIcon(IconNames.Heart)
     basic.pause(2200)
+    ergebnis = 0
+    while (zaehler > 0) {
+        ergebnis = ergebnis + zaehler % 10
+        zaehler = (zaehler - zaehler % 10) / 10
+    }
+    basic.showNumber(ergebnis)
 })
 input.onButtonPressed(Button.A, function () {
     gestartet = 1
     zaehler = 25
     basic.showIcon(IconNames.Heart)
 })
+let ergebnis = 0
 let zaehler = 0
 let gestartet = 0
 basic.showNumber(0)
