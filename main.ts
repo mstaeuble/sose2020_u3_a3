8f8be7b1b6cd04742e030a826325b589b785c347
Markus Stäuble <staeuble@gmail.com>
2020-04-14 20:30:03 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.TiltLeft, function () {
+    if (gestartet == 1) {
+        zaehler += -1
+        if (zaehler < 0) {
+            zaehler = 25
+        }
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    gestartet = 0
+    basic.showNumber(zaehler)
+    basic.pause(2200)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2200)
+})
+input.onButtonPressed(Button.A, function () {
+    gestartet = 1
+    zaehler = 25
+    basic.showIcon(IconNames.Heart)
+})
+let zaehler = 0
+let gestartet = 0
+basic.showNumber(0)
